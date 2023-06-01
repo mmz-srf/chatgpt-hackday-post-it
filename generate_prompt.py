4798f3492890b0cd6fc9c65ac0e2f317e70a71ac
Davide Heller <davide.heller@srf.ch>
2023-06-01 13:38:48 +0200

    add variants

--- generate_prompt.py
+++ generate_prompt.py
@@ -3,14 +3,19 @@ LENGTH = "280"
 ZIELGRUPPE = "junges"
 FORM = "berner-deutsch"
 PERSPEKTIVE = "neutralen"
+VARIANTEN = 3
 
 with open("Text.txt") as f:
     input_text = "".join(f.readlines())
 
 prompt = f"""
    Du bist ein {PLATFORM}-Experte.
-   Kannst du bitte folgenden Text in {LENGTH} Zeichen
+   Kannst du bitte folgenden Text 
+   in {LENGTH} Zeichen
    für ein {ZIELGRUPPE} Publikum 
-   auf Sprache {FORM} mit einer ${PERSPEKTIVE} Perspektive zusammenfassen: [{input_text}]"""
+   auf Sprache {FORM} 
+   mit einer ${PERSPEKTIVE} Perspektive zusammenfassen
+   in {VARIANTEN} Varianten: [{input_text}]"""
 
-print(prompt)
+with open("prompt.txt", 'w') as f:
+    f.write(prompt)
