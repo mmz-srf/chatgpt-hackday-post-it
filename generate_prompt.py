d47067093454a7696dd35b9b10869142eca763af
Davide Heller <davide.heller@srf.ch>
2023-06-01 13:43:16 +0200

    rewrite to include output

--- generate_prompt.py
+++ generate_prompt.py
@@ -1,21 +1,24 @@
 PLATFORM = "twitter"
 LENGTH = "280"
 ZIELGRUPPE = "junges"
-FORM = "berner-deutsch"
+SPRACHE = "berner-deutsch"
 PERSPEKTIVE = "neutralen"
 VARIANTEN = 3
+OUTPUT_FORMAT = "Tweet"
 
 with open("Text.txt") as f:
     input_text = "".join(f.readlines())
 
 prompt = f"""
    Du bist ein {PLATFORM}-Experte.
-   Kannst du bitte folgenden Text 
-   in {LENGTH} Zeichen
+   Kannst du bitte aus folgenden Text 
+   {VARIANTEN} Varianten für ein
+   {OUTPUT_FORMAT}
+   mit {LENGTH} Zeichen
    für ein {ZIELGRUPPE} Publikum 
-   auf Sprache {FORM} 
-   mit einer ${PERSPEKTIVE} Perspektive zusammenfassen
-   in {VARIANTEN} Varianten: [{input_text}]"""
+   auf {SPRACHE} 
+   mit einer {PERSPEKTIVE} Perspektive erzeugen
+   : [{input_text}]"""
 
 with open("prompt.txt", 'w') as f:
     f.write(prompt)
